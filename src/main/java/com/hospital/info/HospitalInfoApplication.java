4329b9f8af2454258221da12d5879d57e922edfa
ohy1023 <zvyg1023@naver.com>
2022-12-08 10:33:11 +0900

    first commit

---
+++ src/main/java/com/hospital/info/HospitalInfoApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital/info/HospitalInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
